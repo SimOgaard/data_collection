37cbd8a613de3060ebd6ada35ad0c098f83ca8ac
Simon Ögaard Jozic <43879969+SimOgaard@users.noreply.github.com>
2021-10-29 11:55:53 +0200

    Update URLs.py

--- URLs.py
+++ URLs.py
@@ -5,16 +5,30 @@ import os
 class URLs:
     @staticmethod
     def Download(url, destination, extract=False, delete_tar=True) -> str:
+        ''' Downloads and extracts google drive url '''
         final_destination:str = destination
         gdown.download(url, destination, quiet=False)
         if extract:
             tar = tarfile.open(destination, "r:gz")
             tar.extractall()
-            final_destination = tar.getnames()
+            final_destination = tar.getnames()[0]
             tar.close()
             if (delete_tar):
                 os.remove(destination)
         return final_destination
+
+    @staticmethod
+    def MountDrive() -> str:
+        ''' Mounts your drive '''
+        from google.colab import drive
+        drive_directory:str = "/content/drive"
+        drive.mount(drive_directory)
+        return drive_directory
+        
+    @staticmethod
+    def Upload():
+        drive_directory = URLs.MountDrive()
+        print(drive_directory)
 
     @staticmethod
     def Hemnet() -> tuple:
