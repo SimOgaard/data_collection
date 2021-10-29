09eabaee6a38bad099eb34f87ff7ede7dc2e8970
Simon Ögaard Jozic <43879969+SimOgaard@users.noreply.github.com>
2021-10-29 12:19:19 +0200

    Update URLs.py

--- URLs.py
+++ URLs.py
@@ -33,10 +33,10 @@ class URLs:
         if not os.path.exists(full_dir):
             os.mkdir(full_dir)
 
-        with tarfile.open(full_dir + full_dir.split("/")[-1].split(".")[0], "w:gz") as tar:
+        full_dir += full_dir.split("/")[-1].split(".")[0]
+        print(full_dir)
+        with tarfile.open(full_dir, "w:gz") as tar:
             tar.add(upload_file_dir, arcname=os.path.basename(upload_file_dir))
-
-        print(drive_directory)
 
     @staticmethod
     def Hemnet() -> tuple:
